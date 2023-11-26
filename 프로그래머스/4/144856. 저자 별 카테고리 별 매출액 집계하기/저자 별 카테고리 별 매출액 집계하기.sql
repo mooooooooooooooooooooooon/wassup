8bd652d80b011ae49c3f1094185c9bb5a8bad241
mooooooooooooooooooooooon <151701231+mooooooooooooooooooooooon@users.noreply.github.com>
2023-11-26 15:58:12 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,20 +1,29 @@
-SELECT
-    b.author_id,
-    a.author_name,
-    b.category,
-    SUM(b.price * s.sales) AS total_sales
-FROM
-    book b
-INNER JOIN
-    author a ON b.author_id = a.author_id
-INNER JOIN
-    book_sales s ON b.book_id = s.book_id
-WHERE
-    s.sales_date BETWEEN '2022-01-01' AND '2022-01-31'
-GROUP BY
-    b.author_id,
-    a.author_name,
-    b.category
-ORDER BY
-    b.author_id ASC,
-    b.category DESC;
+-- 코드를 입력하세요
+select
+    t2.author_id,
+    t2.author_name,
+    t2.category,
+    t2.total_sales
+from
+    (SELECT
+         b.author_id,
+            a.author_name,
+            b.category,
+        sum(b.price * s.sales) as total_sales
+    from
+        book b
+    inner join
+        author a
+    on
+        b.author_id = a.author_id
+    inner join
+        book_sales s
+    on
+        b.book_id = s.book_id
+    where
+        s.sales_date between '2022-01-01' and '2022-01-31'
+    group by
+        b.book_id) t2
+order by
+    t2.author_id asc,
+    t2.category desc
